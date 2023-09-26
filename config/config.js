3694522f0a4c48302423bf6dd970f883f6f9a4e8
Robert Christian Obias <robertchristianobias@gmail.com>
2023-09-26 18:19:28 +0800

    setup mongodb

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: '"mongodb+srv://jitera:McDYNqZsBSPWKdQv@cluster0.hbiz9mu.mongodb.net/?retryWrites=true&w=majority',
+  mongoUri: 'mongodb+srv://jitera:McDYNqZsBSPWKdQv@cluster0.hbiz9mu.mongodb.net/mernproject?retryWrites=true&w=majority',
 }
 
 export default config
